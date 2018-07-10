3bd337194e0237caf09dd361e908220506071217
Paul Fitzgerald <paulfitz99@gmail.com>
2018-07-10 18:06:28 +0100

    add chalk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-const got = require('got');
-const terminalImage = require('term-image');
+const got = require('got')
+const terminalImage = require('term-image')
+const chalk = require('chalk')
 const { version } = require('./package.json')
 const args = process.argv.slice(2)[0];
 
@@ -29,11 +30,11 @@ const getGif = () => {
   (async () => {
     const {body} = await got(`https://api.giphy.com/v1/gifs/search?api_key=p1gIrzabo2nzwNhZblbhIp4x9Xp8zYcn&q=${args}&limit=1&offset=0&rating=G&lang=en`);
     const json = JSON.parse(body);
-    console.log('Fetching.... 🌍')
+    console.log(chalk.cyan('Fetching.... 🌍'))
     let fetchGif = async () => {
-        console.log('Cooking.... 👨‍🍳')
+        console.log(chalk.yellow('Cooking.... 👨‍🍳'))
         const { body } = await got(json.data[0].images.original.url, { encoding: null });
-        console.log('Here we go.... 💃')
+        console.log(chalk.red('Here we go.... 💃'))
         console.log(await terminalImage.buffer(body, {height: '50%'}));
     };
     fetchGif();
