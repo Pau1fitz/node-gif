4ee93c69dfbcb3670a5ffa95e6a50db4e82967bf
Paul Fitzgerald <paulfitz99@gmail.com>
2018-07-10 08:00:18 +0100

    create readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# node-gif
+
+> Show gifs in the terminal
+
+Works in any terminal that supports colors.
+
+<img src="screenshot.png" width="1082">
+
+
+## Install
+
+```
+$ npm install -g node-gif
+```
+
+## Usage
+
+```js
+node-gif [gifSearch]
+```
+
+## Example
+
+```js
+node-gif funny
+```
+
+## License
+
+MIT © Paul Fitzgerald
--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const program = require('commander');
+#!/usr/bin/env node
 const got = require('got');
 const terminalImage = require('term-image');
+const { version } = require('./package.json')
+const args = process.argv.slice(2)[0];
 
-program
-  .version('0.0.1')
-  .description('Node Gif');
+const menu = {
+  main: `
+    node-gif [gifName]
 
-program
-  .command('fetchGif')
-  .description('Get Image')
-  .action((query) => {
-      (async () => {
-        const {body} = await got(`https://api.giphy.com/v1/gifs/search?api_key=p1gIrzabo2nzwNhZblbhIp4x9Xp8zYcn&q=${query}&limit=1&offset=0&rating=G&lang=en`);
-        const json = JSON.parse(body);
-        console.log('Fetching.... 🌍')
-        let fetchGif = async () => {
-            console.log('Cooking.... 👨‍🍳')
-            const { body } = await got(json.data[0].images.original.url, { encoding: null });
-            console.log(await terminalImage.buffer(body, {height: '50%'}));
-        };
-        fetchGif();
-      })()
-      
-});
+    gifName ............ show gif based on input
+    version ............ show package version
+    help ............... show help menu for a command
+    
+    `
+}
 
-program.parse(process.argv);
+if(args === '-v' || args === '--version') {
+  console.log(`v${version}`);
+  return;
+}
+
+if(args === '-h' || args === '--help') {
+  console.log(menu.main);
+  return;
+}
+
+const getGif = () => {
+  (async () => {
+    const {body} = await got(`https://api.giphy.com/v1/gifs/search?api_key=p1gIrzabo2nzwNhZblbhIp4x9Xp8zYcn&q=${args}&limit=1&offset=0&rating=G&lang=en`);
+    const json = JSON.parse(body);
+    console.log('Fetching.... 🌍')
+    let fetchGif = async () => {
+        console.log('Cooking.... 👨‍🍳')
+        const { body } = await got(json.data[0].images.original.url, { encoding: null });
+        console.log('Here we go.... 💃')
+        console.log(await terminalImage.buffer(body, {height: '50%'}));
+    };
+    fetchGif();
+  })()
+}
+
+getGif();
