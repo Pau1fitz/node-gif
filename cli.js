739072a7b72cd5422d7ab9a5164d2aac03a9a012
Paul Fitzgerald <paulfitz99@gmail.com>
2019-12-17 15:32:50 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const terminalImage = require('term-image')
 const chalk = require('chalk')
 const CFonts = require('cfonts')
 const { version } = require('./package.json')
-const args = process.argv.slice(2)[0];
+const args = process.argv.slice(2)[0]
 
 const menu = {
   main: `
@@ -19,12 +19,12 @@ const menu = {
 
 if(args === '-v' || args === '--version') {
   console.log(`v${version}`);
-  return;
+  return
 }
 
 if(args === '-h' || args === '--help') {
   console.log(menu.main);
-  return;
+  return
 }
 
 CFonts.say('node-gif', {
@@ -36,21 +36,21 @@ CFonts.say('node-gif', {
 	lineHeight: 1,              			// define the line height
 	space: true,                			// define if the output text should have empty lines on top and on the bottom
 	maxLength: '0',             			// define how many character can be on one line
-});
+})
 
 const getGif = () => {
   (async () => {
     const { body } = await got(`https://api.giphy.com/v1/gifs/search?api_key=p1gIrzabo2nzwNhZblbhIp4x9Xp8zYcn&q=${args}&limit=1&offset=0&rating=G&lang=en`);
-    const json = JSON.parse(body);
+    const json = JSON.parse(body)
     console.log(chalk.cyan('Fetching.... 🌍'))
     const fetchGif = async () => {
         console.log(chalk.yellow('Cooking.... 👨‍🍳'))
-        const { body } = await got(json.data[0].images.original.url, { encoding: null });
+        const { body } = await got(json.data[0].images.original.url, { encoding: null })
         console.log(chalk.red('Here we go.... 💃'))
-        console.log(await terminalImage.buffer(body, {height: '50%'}));
+        console.log(await terminalImage.buffer(body, {height: '50%'}))
     };
-    fetchGif();
+    fetchGif()
   })()
 }
 
-getGif();
+getGif()
